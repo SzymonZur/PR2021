5ce9ab50e844b62bcb1b2e45f1781f572b4a1fcc
Szymon Żur <szymon.zur2@gmail.com>
2021-05-31 19:11:47 +0200

    Update function for view users to invate (not show actual invated)

--- controllers/getPolls.js
+++ controllers/getPolls.js
@@ -1,5 +1,6 @@
 const Polls = require('../models/Survey');
 const Users = require('../models/User');
+const Conn = require('../models/Conn_US');
 
 
 exports.list = function (req, res) {
@@ -16,15 +17,26 @@ exports.list = function (req, res) {
 };
 
 exports.users = function (req, res) {
-        Users.find({ name: {$ne: req.user.name}}).exec(function (err, users) {
-                if(err) {
-                        return res.send(500,err);
+        Conn.find({ s_id: req.params.id }).exec(function (err, polls) {
+                if(err){
+                        return res.send(500,err)
+                }else{
+                        let items = []
+                        polls.forEach(element => {
+                                items.push(element.u_id)
+                                console.log(element.u_id);
+                        });
+                        Users.find({ name: {$ne: req.user.name}, _id: {$nin: items}}).exec(function (err, users){
+                                if(err){
+                                        return res.send(500,err)
+                                }
+                                res.render("userInvates", {
+                                        id: req.body.currentPoll,
+                                        user: req.user.name,
+                                        allUsers: users,
+                                        pollId: req.params.id
+                                })
+                        })
                 }
-                res.render("userInvates", {
-                        id: req.body.currentPoll,
-                        user: req.user.name,
-                        allUsers: users,
-                        pollId: req.params.id
-                });
-        });
+        })
 };
--- routes/polls.js
+++ routes/polls.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 const polls = require('../controllers/getPolls');
+const Conn = require('../models/Conn_US');
 
 // Your Polls list
 router.get('/yourPolls', ensureAuthenticated, function(req, res) {
@@ -10,11 +11,29 @@ router.get('/yourPolls', ensureAuthenticated, function(req, res) {
 
 router.get('/yourPolls/:id', ensureAuthenticated, function(req, res){
     polls.users(req,res);
-    req.flash('pollId', req.params.id)
 });
 
 
 router.post('/yourPolls/addTo/:id', (req, res) => {
+    const userId = req.body.currentUser
+    let pollId = req.params.id
+    console.log(`ID usera: ${userId}`);
+    console.log(`ID ankiety: ${pollId}`);
+
+    const newConn = new Conn({
+        u_id: req.body.currentUser,
+        s_id: req.params.id
+    });
+
+    newConn.save()
+        .then(Conn => {
+            req.flash('success_msg', 'You are created new Conn');
+            res.redirect('/dashboard');
+        })
+        .catch(err => console.log(err));
+})
+
+router.post('/yourPolls/delete/:id', (req, res) => {
     const userId = req.body.currentUser
     let pollId = req.params.id
     console.log(`ID usera: ${userId}`);
